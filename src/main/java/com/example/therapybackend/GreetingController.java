deecb2f96f2e66ee805b694beb981f422d19e52a
danielnathaniel <danielyaghoobian@gmail.com>
2023-12-14 15:16:09 -0800

    refactored code to hide api key and tested

--- src/main/java/com/example/therapybackend/GreetingController.java
+++ src/main/java/com/example/therapybackend/GreetingController.java
@@ -1,21 +1,36 @@
 package com.example.therapybackend;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.web.bind.annotation.*;
-
 import com.lilittlecat.chatgpt.offical.ChatGPT;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class GreetingController {
-
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private final ChatGPT chatGPT;
 
-    @RequestMapping(value = "/greeting", method = RequestMethod.POST, consumes = "text/plain")
+    public GreetingController() {
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("MY_API_KEY");
+        if (apiKey == null) {
+            throw new IllegalStateException("API Key not found in environment variables");
+        }
+        this.chatGPT = new ChatGPT(apiKey);
+    }
+
+    @RequestMapping(
+            value = {"/greeting"},
+            method = {RequestMethod.POST},
+            consumes = {"text/plain"}
+    )
     public String greeting(@RequestBody String payload) {
-        ChatGPT chatGPT = new ChatGPT("");
-        String hello = chatGPT.ask(payload);
-        System.out.println(hello); // will be "\n\nHello! How may I assist you today?"
+        String hello = this.chatGPT.ask(payload);
+        System.out.println(hello);
         return hello;
     }
 }
